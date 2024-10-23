36f536bc538d0409ecf8d0a7f40b86a5466935c1
Monica-clo <mmoni1141@gmail.com>
2024-10-23 19:01:51 +0530

    Add files via upload

---
+++ LoginData.ts
@@ -0,0 +1,11 @@
+export const LoginData = {
+    logininlink: 'https://automationexercise.com/',
+    validUser: {
+        email: 'mmoni1141@gmail.com',
+        password: 'M@onica@1',
+    },
+    invalidUser: {
+        email: 'invalid@example.com',
+        password: 'invalidPassword',
+    },
+};
